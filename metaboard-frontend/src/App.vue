faed6af51b32a37e8fb7066dd9de93630b7f8a97
Maxime Bourciez <maxime.bourciez@gmail.com>
2026-04-06 19:24:34 +0200

    init (docker): Ajout des fichiers docker pour le projet

--- metaboard-frontend/src/App.vue
+++ metaboard-frontend/src/App.vue
@@ -4,4 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <HelloWorld />
+  <p class="read-the-docs">
+    Click on the Vite and Vue logos to learn more
+  </p>
 </template>
--- metaboard-frontend/vite.config.js
+++ metaboard-frontend/vite.config.js
@@ -4,4 +4,17 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+    hmr: {
+      host: 'localhost',
+      clientPort: 80,
+    },
+  },
 })
